d72b2683c5175120e8a6c2f797c1bef0fc81d60f
anish <anish@anish-ubunutu.hitronhub.home>
2020-09-01 19:58:51 -0700

    Setup the routes

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const express = require("express");
+const app = require("express")();
+const fileupload = require("express-fileupload");
+const {
+  getQuestion,
+  getRandomQuestion,
+  addQuestion,
+  runQuestion,
+} = require("./routes/questions");
 
 const PORT = 5000;
+app.use(fileupload());
 
-const app = express();
+// question routes
+app.get("/question/:id", getQuestion);
+app.get("/question/random", getRandomQuestion);
+app.post("/question/run/:id", runQuestion);
+app.post("/question", addQuestion);
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
-
-app.listen(PORT);
-console.log("app listening at port " + PORT);
+app.listen(PORT, () => console.log("app listening at port " + PORT));
---
+++ routes/questions.js
@@ -0,0 +1,12 @@
+const mongo = require("mongodb");
+require("dotenv").config();
+
+const databaseUrl = process.env.DATABASE_URL;
+
+exports.getQuestion = (req, res) => {};
+
+exports.getRandomQuestion = (req, res) => {};
+
+exports.addQuestion = (req, res) => {};
+
+exports.runQuestion = (req, res) => {};
